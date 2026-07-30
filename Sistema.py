2fa85c99b4af566d0b289e7c31e10a7e462eb8b1
Jesus Salazar <jesus-salazar@correo.unimet.edu.ve>
2026-07-30 15:13:27 -0400

    Avance

---
+++ Sistema.py
@@ -0,0 +1,17 @@
+class Sistema:
+    """_Clase principal que manejara el sistema y tendra todas las opciones y requerimientos _
+    """    
+    def __init__(self):
+        self.nombres_municipios = [
+            "Chacao"
+            "Baruta"
+            "El Hatillo"
+            "Sucre"
+            "Libertador"
+        ]
+        self.municipios = []
+
+    def start(self):
+        print("Sistema")
+        
+    # Hacer menus
---
+++ main.py
@@ -0,0 +1,6 @@
+from Sistema import Sistema
+def main():
+    sistema = Sistema()
+    sistema.start()    
+    
+main()
